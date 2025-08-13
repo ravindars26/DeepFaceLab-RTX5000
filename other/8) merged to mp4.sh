384be3c64d7b3e8e8a51fa2ad0ee53efd756d6b2
Ravinder Kumar <ravindars26@gmail.com>
2025-08-13 14:15:15 +0530

    Update 8) merged to mp4.sh

--- other/8) merged to mp4.sh
+++ other/8) merged to mp4.sh
@@ -15,7 +15,7 @@ python "$DFL_ROOT/main.py" videoed video-from-sequence \
     --lossless
 
 python "$DFL_ROOT/main.py" videoed video-from-sequence \
-    --input-dir "$WORKSPACE/data_dst/merged" \
+    --input-dir "$WORKSPACE/data_dst/merged_mask" \
     --output-file "$WORKSPACE/result_mask.mp4" \
     --reference-file "$WORKSPACE/data_dst.*" \
     --lossless
