2cae084a8098230b9635614e23094000f0f8bff2
Ravinder Kumar <ravindars26@gmail.com>
2025-08-13 08:48:12 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,12 +26,6 @@ mkdir _internal/_e/u
 echo "-------------------------------------------------------"
 echo "Installing CUDA and cuDNN"
 echo "-------------------------------------------------------"
-wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-keyring_1.1-1_all.deb
-sudo dpkg -i cuda-keyring_1.1-1_all.deb
-sudo apt-get update
-sudo apt-get -y install cuda-toolkit-12-8
-rm cuda-keyring_1.1-1_all.deb
-
 # Install cuDNN
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
